ee58d068a19f4980074ecde8100678a4e09054d9
iEdon <admin@iedon.com>
2017-04-11 22:53:59 +0800

    修正OAuth验证

--- URPScoreHelper/OAuth2.h
+++ URPScoreHelper/OAuth2.h
@@ -2,9 +2,9 @@
 #ifndef OAUTH2_H
 #define OAUTH2_H
 
-#define OAUTH2_AUTHENTICATION "https://graph.qq.com/oauth2.0/authorize?response_type=code&client_id=%s&redirect_uri=%s&state=%s&display=mobile"
+#define OAUTH2_AUTHENTICATION "https://graph.qq.com/oauth2.0/authorize?response_type=code&client_id=%s&redirect_uri=%s&state=%s"
 //#define OAUTH2_AUTHENTICATION "https://open.weixin.qq.com/connect/oauth2/authorize?appid=%s&redirect_uri=%s&response_type=code&scope=snsapi_base&state=%s#wechat_redirect"
-//#define OAUTH2_AUTHENTICATION "https://openapi.baidu.com/social/oauth/2.0/authorize?response_type=code&media_type=qqdenglu&client_id=%s&redirect_uri=%s&state=%s&display=mobile"
+//#define OAUTH2_AUTHENTICATION "https://openapi.baidu.com/social/oauth/2.0/authorize?response_type=code&media_type=qqdenglu&client_id=%s&redirect_uri=%s&state=%s"
 //#define OAUTH2_ACCESS_TOKEN "https://api.weixin.qq.com/sns/oauth2/access_token?appid=%s&secret=%s&code=%s&grant_type=authorization_code"
 #define OAUTH2_ACCESS_TOKEN "https://graph.qq.com/oauth2.0/token?grant_type=authorization_code&client_id=%s&client_secret=%s&code=%s&redirect_uri=%s"
 //#define OAUTH2_ACCESS_TOKEN "https://openapi.baidu.com/social/oauth/2.0/token?grant_type=authorization_code&client_id=%s&client_secret=%s&code=%s&redirect_uri=%s"
