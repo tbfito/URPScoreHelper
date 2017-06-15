fa47322c5d639c966522ac9f2e7c49d1d5b70c9a
iEdon <admin@iedon.com>
2017-06-15 15:02:45 +0800

    增加超时检测

--- URPScoreHelper/CrawlFactory.cpp
+++ URPScoreHelper/CrawlFactory.cpp
@@ -41,16 +41,59 @@ bool CrawlRequest(const char *p_rq, char *p_lpvBuffer, int p_iLength, int *p_iTo
 	m_sockaddr.sin_addr.S_un.S_addr = inet_addr(SERVER);
 	m_sockaddr.sin_port = htons(80);
 
+	int error = -1;
+	int len = sizeof(int);
+	timeval tm;
+	fd_set set;
+	unsigned long ul = 1;
+	ioctlsocket(g_so, FIONBIO, &ul); //设置为非阻塞模式
+	bool ret = false;
+
 	int m_iResult = connect(g_so, (sockaddr *)&m_sockaddr, sizeof(m_sockaddr));
 	if (m_iResult == SOCKET_ERROR)
+	{
+		tm.tv_sec = 1;
+		tm.tv_usec = 0;
+		FD_ZERO(&set);
+		FD_SET(g_so, &set);
+		if (select(g_so + 1, NULL, &set, NULL, &tm) > 0)
+		{
+			getsockopt(g_so, SOL_SOCKET, SO_ERROR, (char *)&error, /*(socklen_t *)*/&len);
+			if (error == 0)
+				ret = true;
+			else
+				ret = false;
+		}
+		else
+			ret = false;
+	}
+	else
+	{
+		ret = true;
+	}
+	ul = 0;
+	ioctlsocket(g_so, FIONBIO, &ul);
+	if(!ret)
 	{
 		closesocket(g_so);
 		WSACleanup();
 		puts("Status: 500 Internal Server Error");
-		Error("<p>连接教务系统服务器失败！</p>");
+		int errid = WSAGetLastError();
+		char buff[10] = { 0 };
+		_itoa(errid, buff,10);
+		std::string err_msg("<p><b>连接学校服务器失败！</b></p><p>OS代码: (");
+		err_msg.append(buff);
+		err_msg.append(") ");
+		char *wsamsg = (char *)malloc(MAX_PATH);
+		FormatMessageA(4096, NULL, errid, NULL, wsamsg, MAX_PATH, NULL);
+		err_msg.append(wsamsg);
+		if (errid = 0)
+			err_msg.append("连接超时");
+		err_msg.append("</p><p>可能学校服务器挂了，我能怎么办，我也很绝望？</p>");
+		free(wsamsg);
+		Error((char *)err_msg.c_str());
 		return false;
 	}
-
 	// 发送首页请求，相当于访问 http://SERVER_IP/ 。
 	m_iResult = send(g_so, p_rq, strlen(p_rq), 0);
 	if (m_iResult != strlen(p_rq))
