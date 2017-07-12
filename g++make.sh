6b5fa29291faeda2f10a0e31ac553cd0f640eb0c
iEdon <admin@iedon.com>
2017-07-12 21:34:30 +0800

    更新命令

--- g++make.sh
+++ g++make.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd URPScoreHelper
 sudo apt-get install libfcgi-dev libfcgi curl libcurl4-openssl-dev libsqlite3-dev
-g++ -o URPScoreHelper.exe -O2 -std=c++11 -fpermissive -Woverflow -Wwrite-strings stdafx.cpp General.cpp CCurlTask.cpp StringHelper.cpp Encrypt.cpp ini.cpp INIReader.cpp OAuth2.cpp URPRequests.cpp URPScoreHelper.cpp -lcurl -lfcgi++ -lrt -lfcgi -lsqlite3
+g++ -o URPScoreHelper.exe -O2 -std=c++11 -fpermissive -Woverflow -Wwrite-strings stdafx.cpp General.cpp CCurlTask.cpp StringHelper.cpp Encrypt.cpp ini.cpp INIReader.cpp OAuth2.cpp URPRequests.cpp URPScoreHelper.cpp -lcurl -lfcgi++ -lfcgi -lsqlite3
