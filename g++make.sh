b7a3344b87a06bf15ea66552125b3d71ea2303b5
iEdon <admin@iedon.com>
2017-07-12 16:13:26 +0800

    增加跨平台编译脚本

---
+++ g++make.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+cd URPScoreHelper
+g++ -o URPScoreHelper.exe -O2 -std=c++11 -fpermissive -Woverflow -Wwrite-strings stdafx.cpp General.cpp CCurlTask.cpp StringHelper.cpp Encrypt.cpp ini.cpp INIReader.cpp OAuth2.cpp URPRequests.cpp URPScoreHelper.cpp -L "../Unix_libs" -lcurl -lfcgi -lsqlite3
