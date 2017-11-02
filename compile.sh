90d2c53472e4f8bfee3a5819e3d0df21b0ae829c
iEdon <admin@iedon.com>
2017-11-02 23:43:54 +0800

    修改编译指令

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd URPScoreHelper
 sudo apt-get install libfcgi-dev libfcgi curl libcurl4-openssl-dev libsqlite3-dev
-gcc -o URPScoreHelper -O2 -std=c++11 stdafx.cpp gbkutf8.cpp General.cpp CCurlTask.cpp StringHelper.cpp Encrypt.cpp Admin.cpp OAuth2.cpp URPRequests.cpp URPScoreHelper.cpp main.cpp -lstdc++ -lcurl -lfcgi++ -lfcgi -lsqlite3
+gcc -o URPScoreHelper -O2 -std=c++11 stdafx.cpp gbkutf8.cpp General.cpp CCurlTask.cpp StringHelper.cpp Encrypt.cpp Admin.cpp OAuth2.cpp URPRequests.cpp URPScoreHelper.cpp AES.cpp INI_Reader.cpp main.cpp -lstdc++ -lcurl -lfcgi++ -lfcgi -lsqlite3
